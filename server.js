53f1d98d8152d577cdc193fb480f0798cab42341
yudhaPratama <muhyudhapratama55@gmail.com>
2024-06-06 21:59:53 +0700

    [FEATURE] add feature development

---
+++ server.js
@@ -0,0 +1,16 @@
+import app from "./app.js"
+
+const PORT = process.env.PORT || 3000
+
+const main = async () => {
+    try{
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
+
+    }catch(e){
+        console.error(e)
+    }
+}
+
+main()
