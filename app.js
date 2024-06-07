9308f2c78d7466d0ff9063a0206cf2b24e9608c1
yudhaPratama <muhyudhapratama55@gmail.com>
2024-06-07 12:25:02 +0700

    [IMPROVE] add routes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { authRouter, cartRouter, categoryRouter, productRouter, userRouter } from "./routes/index.js"
 
 dotenv.config()
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/auth", authRouter)
+app.use("/api/users", userRouter)
+app.use("/api/products", productRouter)
+app.use("/api/categories", categoryRouter)
+app.use("/api/carts", cartRouter)
 
 export default app
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+import authRouter from "./auth.routes.js";
+import userRouter from "./user.routes.js";
+import productRouter from "./product.routes.js"
+import categoryRouter from "./category.routes.js"
+import cartRouter from "./cart.routes.js"
+
+export {
+    authRouter,
+    userRouter,
+    productRouter,
+    cartRouter,
+    categoryRouter
+}
