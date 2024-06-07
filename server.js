0d46e5f608d2ca624884d103b33160a8cfd3c4db
yudhaPratama <muhyudhapratama55@gmail.com>
2024-06-07 11:03:20 +0700

    [IMPROVE] add database config with prisma and mongodb

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 import app from "./app.js"
-
+import prisma from "./models/index.js"
 const PORT = process.env.PORT || 3000
 
 const main = async () => {
     try{
+        await prisma.$connect();
+        console.log("Connected to the database")
+
         app.listen(PORT, () => {
             console.log(`Server is running on http://localhost:${PORT}`)
         })
-
     }catch(e){
         console.error(e)
     }
